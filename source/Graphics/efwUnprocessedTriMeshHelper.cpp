750a262c0e8a110508389d93a0ad38402313645a
Bruno Evangelista <me@here>
2012-10-01 22:19:10 -0700

    Adding methods to generate AABB and  merge duplicated vertices

--- source/Graphics/efwUnprocessedTriMeshHelper.cpp
+++ source/Graphics/efwUnprocessedTriMeshHelper.cpp
@@ -1,21 +1,57 @@
 #include "Graphics/efwUnprocessedTriMeshHelper.h"
 #include "Graphics/efwUnprocessedTriMesh.h"
 #include "Math/efwMath.h"
+#include "Math/efwVectorMath.h"
+
+#include <algorithm>
+#include <map>
+#include <vector>
 
 using namespace efw::Graphics;
+using namespace efw::Math;
+
+void UnprocessedTriMeshHelper::GenerateAABoundingBox(AABoundingBox* out, const UnprocessedTriMesh& mesh)
+{
+	if (out == NULL)
+		return;
+
+	if (mesh.vertexCount == 0)
+	{
+		out->min = Vec3f::kZero;
+		out->max = Vec3f::kZero;
+		return;
+	}
+
+	// Calculate bounding sphere center
+	Vec3f min = Vec3f(FLT_MAX);
+	Vec3f max = Vec3f(-FLT_MAX);
+
+	int32_t vertexStride = mesh.vertexStride;
+	float* positions = (float*)( (uint8_t*)mesh.vertexData + mesh.vertexAttributes[VertexAttributes::kPosition].offset );
+	for (int32_t i=0; i<mesh.vertexCount; ++i)
+	{
+		Vec3f position = Vec3f(positions);
+		min = Vec3Min(min, position);
+		max = Vec3Max(max, position);
+		positions += vertexStride/sizeof(float);
+	}
+
+	out->min = min;
+	out->max = max;
+}
 
 
 void UnprocessedTriMeshHelper::GenerateBoundingSphere(float* outBoudingSphere, const UnprocessedTriMesh& mesh)
 {
 	if (outBoudingSphere == NULL)
 		return;
+
 	if (mesh.vertexCount == 0)
 	{
 		outBoudingSphere[0] = outBoudingSphere[1] = outBoudingSphere[2] = outBoudingSphere[3] = 0;
 		return;
 	}
 
-	// Calculate bounding sphere center
 	float minX = FLT_MAX, minY = FLT_MAX, minZ = FLT_MAX;
 	float maxX = -FLT_MAX, maxY = -FLT_MAX, maxZ = -FLT_MAX;
 
@@ -102,4 +138,197 @@ void UnprocessedTriMeshHelper::MergeBoundingSphere(float* outBoudingSphere, cons
 		outBoudingSphere[1] == outBoudingSphere[1] && 
 		outBoudingSphere[2] == outBoudingSphere[2] &&
 		outBoudingSphere[3] == outBoudingSphere[3]);
+}
+
+bool DuplicatedVerticesSort(const std::vector<int32_t>& a, const std::vector<int32_t>& b)
+{
+	return (a.size()>b.size());
+}
+
+void UnprocessedTriMeshHelper::MergeDuplicatedVertices(UnprocessedTriMesh* mesh, float positionDeltaThreashold, int32_t mergeDuplicateFlags)
+{
+	if (mesh == NULL)
+		return;
+
+	const int32_t kOctreeAxisPartions = 32;
+	const int32_t kOctreePartitions = kOctreeAxisPartions*kOctreeAxisPartions*kOctreeAxisPartions;
+
+	AABoundingBox boundingBox;
+	GenerateAABoundingBox(&boundingBox, *mesh);
+	Vec3f boundingBoxSize = boundingBox.max - boundingBox.min;
+	boundingBoxSize = Vec3Max(boundingBoxSize * 1.01f, Vec3f(1.0f));
+	Vec3f invOctreePartitionSize = Vec3f( 1.0f / (boundingBoxSize/kOctreeAxisPartions) );
+
+	std::vector<int32_t>* octree = (std::vector<int32_t>*)memalign(16, kOctreePartitions*sizeof(std::vector<int32_t>));
+	memset(octree, 0, kOctreePartitions*sizeof(std::vector<int32_t>));
+	//for (int32_t i=0; i<kOctreePartitions; ++i)
+	//	octree[i] = std::vector<int32_t>();
+
+	int32_t vertexComponents = mesh->vertexStride/sizeof(float);
+	float* positions = (float*)( (uint8_t*)mesh->vertexData + mesh->vertexAttributes[VertexAttributes::kPosition].offset );
+	float* normals = (mesh->vertexAttributes[VertexAttributes::kNormal].componentCount == 0 || (mergeDuplicateFlags & MergeVertexFlags::kTangentPlane_Exact) == 0)? NULL :
+		(float*)( (uint8_t*)mesh->vertexData + mesh->vertexAttributes[VertexAttributes::kNormal].offset );
+	float* uvs = (mesh->vertexAttributes[VertexAttributes::kUv0].componentCount == 0 || (mergeDuplicateFlags & MergeVertexFlags::kUvw_Exact) == 0)? NULL :
+		(float*)( (uint8_t*)mesh->vertexData + mesh->vertexAttributes[VertexAttributes::kUv0].offset );
+
+	// Split vertex indices into octree-buckets
+	for (int32_t i=0;i<mesh->vertexCount;++i)
+	{
+		Vec3f position = Vec3f( &positions[i*vertexComponents] );
+
+		Vec3f deltaVec = position - boundingBox.min;
+		deltaVec *= invOctreePartitionSize;
+
+		Vec3f neighborsVec = Vec3Floor(deltaVec + Vec3f(0.5f));
+		deltaVec = Vec3Floor(deltaVec);
+
+		float deltas[3];
+		Vec3GetFloats(deltas, deltaVec);
+		int32_t partitionIndex = (int32_t)deltas[0] + (int32_t)deltas[1] * kOctreeAxisPartions + (int32_t)deltas[2] * kOctreeAxisPartions*kOctreeAxisPartions;
+		octree[partitionIndex].push_back(i);
+
+		// Get neighbor index
+		neighborsVec -= deltaVec;
+		neighborsVec = neighborsVec*Vec3f(2.0f) -Vec3f(1.0f);
+		neighborsVec += deltaVec;
+		float neighbors[3];
+		Vec3GetFloats(neighbors, neighborsVec);
+
+		int32_t partitionIndexNeighborX = (int32_t)neighbors[0] + (int32_t)deltas[1] * kOctreeAxisPartions + (int32_t)deltas[2] * kOctreeAxisPartions*kOctreeAxisPartions;
+		int32_t partitionIndexNeighborY = (int32_t)deltas[0] + (int32_t)neighbors[1] * kOctreeAxisPartions + (int32_t)deltas[2] * kOctreeAxisPartions*kOctreeAxisPartions;
+		int32_t partitionIndexNeighborZ = (int32_t)deltas[0] + (int32_t)deltas[1] * kOctreeAxisPartions + (int32_t)neighbors[2] * kOctreeAxisPartions*kOctreeAxisPartions;
+		int32_t partitionIndexNeighborXY = (int32_t)neighbors[0] + (int32_t)neighbors[1] * kOctreeAxisPartions + (int32_t)deltas[2] * kOctreeAxisPartions*kOctreeAxisPartions;
+		int32_t partitionIndexNeighborXZ = (int32_t)neighbors[0] + (int32_t)deltas[1] * kOctreeAxisPartions + (int32_t)neighbors[2] * kOctreeAxisPartions*kOctreeAxisPartions;
+		int32_t partitionIndexNeighborYZ = (int32_t)deltas[0] + (int32_t)neighbors[1] * kOctreeAxisPartions + (int32_t)neighbors[2] * kOctreeAxisPartions*kOctreeAxisPartions;
+		int32_t partitionIndexNeighborXYZ = (int32_t)neighbors[0] + (int32_t)neighbors[1] * kOctreeAxisPartions + (int32_t)neighbors[2] * kOctreeAxisPartions*kOctreeAxisPartions;
+
+		bool passX = (neighbors[0] >=0 && neighbors[0] < kOctreeAxisPartions);
+		bool passY = (neighbors[1] >=0 && neighbors[1] < kOctreeAxisPartions);
+		bool passZ = (neighbors[2] >=0 && neighbors[2] < kOctreeAxisPartions);
+		if (passX) octree[partitionIndexNeighborX].push_back(i);
+		if (passY) octree[partitionIndexNeighborY].push_back(i);
+		if (passZ) octree[partitionIndexNeighborZ].push_back(i);
+		if (passX&passY) octree[partitionIndexNeighborXY].push_back(i);
+		if (passX&passZ) octree[partitionIndexNeighborXZ].push_back(i);
+		if (passY&passZ) octree[partitionIndexNeighborYZ].push_back(i);
+		if (passX&passY&passZ) octree[partitionIndexNeighborXYZ].push_back(i);
+	}
+
+	// For each vertex, store an array of indices of other vertices that are near and can be merged
+	int32_t duplicateIndex = 0;
+	std::vector<std::vector<int32_t>> duplicates;
+	duplicates.resize(mesh->vertexCount);
+	for (int32_t i=0; i<kOctreePartitions; ++i)
+	{
+		for (uint32_t j=0; j<octree[i].size(); ++j)
+		{
+			int32_t testIndex = octree[i][j];
+			duplicates[testIndex].push_back( testIndex );
+			Vec3f testPosition = Vec3f( &positions[testIndex*vertexComponents] );
+			float* testNormal = (normals != NULL)? &normals[testIndex*vertexComponents] : NULL;
+			float* testUv0 = (uvs != NULL)? &uvs[testIndex*vertexComponents] : NULL;
+
+			for (uint32_t k=j+1; k<octree[i].size(); ++k)
+			{
+				int32_t index = octree[i][k];
+				Vec3f position = Vec3f( &positions[index*vertexComponents] );
+				float* normal = (normals != NULL)? &normals[index*vertexComponents] : NULL;
+				float* uv0 = (uvs != NULL)? &uvs[index*vertexComponents] : NULL;
+				
+				// check if position[j] == position[k] (using some EPSILON)
+				// if so add new position = (position[j]+position[k])/2
+				// Here we also handle Normal, Tagent and Uv differences
+				
+				// TODO Check for normals to average
+				// TODO
+				
+				Vec3f distanceSquared = Vec3LengthSquared(position - testPosition);
+				if (distanceSquared.X() < (positionDeltaThreashold*positionDeltaThreashold) &&
+					(normals == NULL || (testNormal[0] == normal[0] && testNormal[1] == normal[1] && testNormal[2] == normal[2])) &&
+					(uvs == NULL || (testUv0[0] == uv0[0] && testUv0[1] == uv0[1]))
+					)
+				{
+					duplicates[testIndex].push_back(index);
+					duplicates[index].push_back(testIndex);
+				}
+			}
+		}
+	}
+	SAFE_ALIGNED_FREE(octree);
+
+	// Sort duplicates based on count
+	std::sort(duplicates.begin(), duplicates.end(), DuplicatedVerticesSort);
+
+	bool* usedTable = (bool*)memalign(16, mesh->vertexCount * sizeof(bool));
+	memset(usedTable, 0, sizeof(bool) * mesh->vertexCount);
+	std::map<uint32_t, uint32_t> indexMap = std::map<uint32_t, uint32_t>();
+	
+	// Create new empty vertex list
+	uint32_t newVertexCount = 0;
+	float* newVertexData = (float*)memalign(16, mesh->vertexCount * mesh->vertexStride);
+	for (int32_t i=0; i<vertexComponents; ++i)
+		newVertexData[i] = 0.0f;
+
+	for (uint32_t i=0; i<duplicates.size(); ++i)
+	{
+		int32_t mergeCount = 0;
+		
+		for (uint32_t j=0; j<duplicates[i].size(); ++j)
+		{
+			int vertexIndex = duplicates[i][j];
+			if (usedTable[vertexIndex])
+				continue;
+
+			// TODO Improve this
+			// Add data from current vertex to the global one
+			float* meshVertexData = (float*)mesh->vertexData;
+			for (int32_t k=0; k<vertexComponents; ++k)
+				newVertexData[newVertexCount*vertexComponents+k] += meshVertexData[vertexIndex*vertexComponents+k];
+
+			// Create remap for index
+			indexMap.insert( std::pair<uint32_t, uint32_t>(vertexIndex, newVertexCount) );
+			
+			usedTable[vertexIndex] = true;
+			mergeCount++;
+		}
+
+		if (mergeCount > 0)
+		{
+			for (int32_t k=0; k<vertexComponents; ++k)
+				newVertexData[newVertexCount*vertexComponents+k] /= mergeCount;
+			newVertexCount++;
+		}
+	}
+	duplicates.clear();
+	SAFE_ALIGNED_FREE(usedTable);
+
+	static uint32_t totalCount = 0;
+	static uint32_t totalDCount = 0;
+	totalCount += mesh->vertexCount;
+	totalDCount += mesh->vertexCount - newVertexCount;
+
+	int32_t duplicatedVertexCount = mesh->vertexCount - newVertexCount;
+	printf("VCount: %d  - Duplications: %d\r\n", mesh->vertexCount, duplicatedVertexCount);
+	//printf("VCount: %d  - Duplications: %d\r\n", totalCount, totalDCount);
+
+	// Replace old vertex data with new one
+	if (newVertexCount != mesh->vertexCount)
+	{
+		SAFE_ALIGNED_FREE(mesh->vertexData);
+	
+		float* newVertexDataCompact = (float*)memalign(16, newVertexCount * mesh->vertexStride);
+		memcpy(newVertexDataCompact, newVertexData, newVertexCount * mesh->vertexStride);
+		mesh->vertexData = newVertexDataCompact;
+		mesh->vertexCount = newVertexCount;
+
+		// Adjust index list
+		uint32_t* indices = (uint32_t*)mesh->indexData;
+		for (int32_t i=0; i<mesh->indexCount; ++i)
+		{
+			uint16_t currentIndex = indices[i];
+			indices[i] = indexMap[currentIndex];
+		}
+	}
+
+	SAFE_ALIGNED_FREE(newVertexData);
 }
--- source/Graphics/efwUnprocessedTriMeshHelper.h
+++ source/Graphics/efwUnprocessedTriMeshHelper.h
@@ -1,32 +1,62 @@
 /**
+ * Copyright (C) 2012 Bruno P. Evangelista. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * 
- * @author Bruno Evangelista
- *
- * THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
  */
 #pragma once
 
 #include "Foundation/efwPlatform.h"
+#include "Graphics/efwUnprocessedTriMesh.h"
+
+#include "Math/efwVectorMath.h"
 
 namespace efw
 {
 
 namespace Graphics
 {
-	// Forward declarations
-	struct UnprocessedTriMesh;
+	// TODO BE Move it to somewhere else (physics?)
+	struct AABoundingBox
+	{
+		Math::Vec3f min;
+		Math::Vec3f max;
+	};
+
+	struct BoundingSphere
+	{
+		float center[3];
+		float radius;
+	};
+
+	namespace MergeVertexFlags
+	{
+		const int32_t kTangentPlane_Exact				= 1<<1;
+		const int32_t kTangentPlane_Average				= 1<<2;
+		const int32_t kTangentPlane_AverageUniques		= 1<<3;
+
+		const int32_t kUvw_Exact						= 1<<4;
+		const int32_t kUvw_Average						= 1<<5;
+		const int32_t kUvw_AveragaUniques				= 1<<6;
+	}
+
 
 	namespace UnprocessedTriMeshHelper
 	{
+		void GenerateAABoundingBox(AABoundingBox* outBoudingBox, const UnprocessedTriMesh& mesh);
+
 		void GenerateBoundingSphere(float* outBoudingSphere, const UnprocessedTriMesh& mesh);
 		void MergeBoundingSphere(float* outBoudingSphere, const float* boudingSphere1, const float* boudingSphere2);
+
+		void MergeDuplicatedVertices(UnprocessedTriMesh* mesh, float positionDeltaThreashold, int32_t mergeDuplicateFlags);
 	}
 }
+
 }
