597a61ce2506b279914a39e3f8ef1fd4723ad09d
Bruno Evangelista <me@here>
2012-10-22 22:32:32 -0700

    Fixed File::GetInfo multi-platform bug

--- source/Foundation/efwFile.cpp
+++ source/Foundation/efwFile.cpp
@@ -1,25 +1,23 @@
 #include "Foundation/efwFile.h"
 
-//#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdio.h>
-
-#if !defined _MSC_VER
-#include <unistd.h>
-#endif
-
 using namespace efw;
 
 int32_t File::GetInfo(FileInfo* outFileInfo, const char* filename)
 {
 	memset(outFileInfo, 0, sizeof(FileInfo));
 
-	struct __stat64 stats;
-	int result = _stat64(filename, &stats);
+#ifdef _MSC_VER
+	struct __stat64 fileStats;
+	int result = _stat64(filename, &fileStats);
+#else
+	struct stat fileStats;
+	int result = stat(filename, &fileStats);
+#endif
+	
 	if (result == 0)
 	{
 		outFileInfo->exists = true;
-		outFileInfo->size = (size_t)stats.st_size;
+		outFileInfo->size = (size_t)fileStats.st_size;
 		return efwErrs::kOk;
 	}
 
